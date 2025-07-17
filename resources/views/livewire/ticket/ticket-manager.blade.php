92c0e0ff4a8063236c6010a70303a6bb329b33f5
David <daviddevyauri@gmail.com>
2025-07-17 10:32:21 -0500

    fix: Remove unnecessary user greeting and clean up area check in ticket manager view

--- resources/views/livewire/ticket/ticket-manager.blade.php
+++ resources/views/livewire/ticket/ticket-manager.blade.php
@@ -13,10 +13,7 @@
     <!-- Título y Acción -->
     <div class="flex justify-between items-center">
         <h1 class="text-2xl font-bold text-gray-900">Gestión de Tickets</h1>
-        <h1>Hola</h1>
-        <h1>{{ auth()->user()?->area }}</h1>
-        @if(Auth()->user()?->area?->parent_id == 1)
-        {{ Auth()->user()?->area?->parent_id }}
+        @if(auth()->user()?->area?->parent_id == 1)
         {{-- <flux:button icon="plus" class="bg-black" variant="primary" wire:click="$dispatch('abrirModalCreacionTicket')">
             Crear Nuevo Ticket
         </flux:button> --}}
