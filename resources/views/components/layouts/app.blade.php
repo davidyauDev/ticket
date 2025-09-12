60f7f565b809ad2c779a1bf1c9b41fbd9f07eff2
David <daviddevyauri@gmail.com>
2025-09-12 12:39:08 -0500

    fix

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,62 +1,51 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport"
-    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <title>
-    Mesa de Ayuda
-  </title>
-  <link rel="icon" type="image/svg+xml" href="{{ asset('images/logo-icon.svg') }}">
-  <link rel="preconnect" href="https://fonts.bunny.net">
-  <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/es.js"></script>
+    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Mesa de Ayuda</title>
 
+  <link rel="icon" type="image/svg+xml" href="{{ asset('images/logo-icon.svg') }}">
+
+  <link rel="preconnect" href="https://fonts.bunny.net" crossorigin>
+  <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" media="print" onload="this.media='all'">
+
+  @vite(['resources/css/app.css'])
+
+  <link rel="preload" as="style" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"></noscript>
+
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/apexcharts" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/flatpickr" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/es.js" defer></script>
 
 </head>
 
 <body
-  x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
+  x-data="{ page: 'ecommerce', loaded: true, darkMode: false, stickyMenu: false, sidebarToggle: false, scrollTop: false }"
   x-init="darkMode = JSON.parse(localStorage.getItem('darkMode'));
-$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
-  :class="{ 'dark bg-gray-900': darkMode === true }">
-  <!-- ===== Preloader Start ===== -->
-  <!-- ===== Preloader End ===== -->
+           $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+  :class="{ 'dark bg-gray-900': darkMode }">
 
-  <!-- ===== Page Wrapper Start ===== -->
+
   <div class="flex h-screen overflow-hidden">
-    <!-- ===== Sidebar Start ===== -->
     <x-layouts.app.sidebar />
-    <!-- ===== Sidebar End ===== -->
 
-    <!-- ===== Content Area Start ===== -->
     <div class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
-      <!-- Small Device Overlay Start -->
       <x-layouts.overlay />
-      <!-- Small Device Overlay End -->
-
-      <!-- ===== Header Start ===== -->
       <x-layouts.app.header2 />
-      <!-- ===== Header End ===== -->
-
-      <!-- ===== Main Content Start ===== -->
       <main>
         {{ $slot }}
       </main>
-      <!-- ===== Main Content End ===== -->
     </div>
-    <!-- ===== Content Area End ===== -->
   </div>
   @fluxScripts
-  <!-- ===== Page Wrapper End ===== -->
-</body>
+  @vite(['resources/js/app.js'])
 
+</body>
 </html>
-@vite(['resources/css/app.css', 'resources/js/app.js'])
